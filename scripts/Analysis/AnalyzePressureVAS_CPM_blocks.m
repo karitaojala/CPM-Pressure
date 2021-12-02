d8e6114ce97b7063332a3a87928ec03e1ccb633b
Karita Ojala <k.ojala@uke.de>
2021-12-02 15:16:14 +0100

    Analysis of rating data across/within blocks

--- scripts/Analysis/AnalyzePressureVAS_CPM_blocks.m
+++ scripts/Analysis/AnalyzePressureVAS_CPM_blocks.m
@@ -11,7 +11,7 @@
 % Version notes
 % 1.0
 
-function AnalyzePressureVAS_CPM
+function AnalyzePressureVAS_CPM_blocks
 
 close all; clear all
 
@@ -37,45 +37,44 @@ elseif strcmp(project.phase,'Pilot-04')
 %     phasicStimPressure = [];
 end
 
+rows = ceil(numel(subjects)/5);
+cols = 5;
+figure('Position',[10 10 1800 800]); 
+
+datatable_long = NaN(numel(subjects)*4,4);
+datatable_trial = NaN(numel(subjects)*4*2*9,5); % 4 blocks, 2 trials per block, 9 ratings per trial
+
+sub_row_no = 1;
+sub_row_no_trial = 1;
+    
 for sub = 1:numel(subjects)
     
-    clear ratings_blocks pressure
+    clear ratings_blocks pressure blocks blocktrials conditions
     
     subID = ['sub' sprintf('%03d',subjects(sub))];
     
     path.sub = fullfile(path.data,subID,'pain');
     
     blocks_sub = block_order{sub};
+%     blocks = NaN(4,18);
+%     conditions = NaN(4,18);
     
     row_no = 1;
     
-    for cond = 1:numel(blocks_sub)
+    for block = 1:numel(blocks_sub)
         
         if strcmp(project.phase,'Pilot-02') && subjects(sub) < 11
-            path.datafile =  fullfile(path.sub,[subID '_VAS_rating_block' num2str(cond) '.mat']);
+            path.datafile =  fullfile(path.sub,[subID '_VAS_rating_block' num2str(block) '.mat']);
         else
-            path.datafile =  fullfile(path.sub,[subID '_VAS_rating_block' num2str(cond) '_phasicstim.mat']);
+            path.datafile =  fullfile(path.sub,[subID '_VAS_rating_block' num2str(block) '_phasicstim.mat']);
         end
         path.paramfile = fullfile(path.sub,['parameters_' subID '.mat']);
 
         data = load(path.datafile,'VAS');
         data = data.VAS;
         
-        param = load(path.paramfile,'P');
-        param = param.P;
-        
-        phasicStimPressure(sub,1) = param.pain.CPM.experimentPressure.phasicStim;
-        
-        tonicStimPeak = param.pain.CPM.experimentPressure.tonicStimPeak;
-        tonicStimTrough = param.pain.CPM.experimentPressure.tonicStimTrough;
-        
-        if tonicStimPeak > tonicStimTrough
-            tonicStimPressurePeak(sub,1) = tonicStimPeak;
-            tonicStimPressureTrough(sub,1) = tonicStimTrough;
-        else % some subjects had the values saved the wrong way (a few from the beginning of the pilot)
-            tonicStimPressureTrough(sub,1) = tonicStimPeak;
-            tonicStimPressurePeak(sub,1) = tonicStimTrough;
-        end
+%         param = load(path.paramfile,'P');
+%         param = param.P;
         
         trials = size(data,1);
         if trials > 3; trials = trials(1:3,:); end % 4th trial (if exists) rating for tonic stimulus only (not phasic)
@@ -94,6 +93,9 @@ for sub = 1:numel(subjects)
         end
         
         ratings_blocks(row_no,:) = ratings; %#ok<AGROW>
+        blocks(block,1:numel(ratings)) = block;
+        blocktrials(block,1:numel(ratings)) = 1:numel(ratings);
+        conditions(block,1:numel(ratings)) = blocks_sub(block);
         
         if strcmp(subID,'sub005') && row_no == 1
             ratings_blocks(row_no,stimuli+1:stimuli*2) = NaN;% accidentally overwrote first trial with second trial values
@@ -102,43 +104,21 @@ for sub = 1:numel(subjects)
         
     end
     
-    pressure = ones(length(ratings),2)*phasicStimPressure(sub); %#ok<AGROW>
-    
-%     format long
-%     y = ratings';
-%     x = pressure';
-%     X = [ones(length(x),1) x];
-%     b = X\y;
-%     yCalc = X*b;
-    
-
-%     if plotIndividual
-%         
-%         figure %#ok<UNRCH>
-%         bar(mean(ratings_blocks,2),'LineWidth',1); 
-%         hold on
-%         
-%         xdata = repmat([1 2],size(ratings_blocks,2),1);
-%         jitter_amount = 0.05;
-%         jittered_xdata = xdata + (rand(size(xdata))-0.5)*(2*jitter_amount);
-%         jittered_xdata = jittered_xdata';
-%         
-%         for cond = 1:2
-%             scatter(jittered_xdata(cond,:),ratings_blocks(cond,:),'filled','MarkerEdgeColor','k');
-%         end
-%         
-%         ylim([0 100])
-%         ylabel('VAS pain rating')
-%         set(gca,'xTickLabel', {'Control','Experimental'})
-%         
-% %         legend('Control','Experimental')
-%         title(['Conditioned pain modulation - / ' project.phase ' - ' subID])
-%         
-%     end
+%     pressure = ones(length(ratings),2)*phasicStimPressure(sub); %#ok<AGROW>
     
     ratings_allsubs{sub} = ratings; %#ok<NASGU,AGROW>
-    pressure_allsubs{sub} = pressure; %#ok<NASGU,AGROW>
+%     pressure_allsubs{sub} = pressure; %#ok<NASGU,AGROW>
 
+    datatable_wide(sub,1) = sub;
+    datatable_wide(sub,2:numel(blocks_sub)+1) = nanmean(ratings_blocks,2);
+    
+    datatable_long(sub_row_no:sub_row_no+numel(blocks_sub)-1,1) = sub;
+    datatable_long(sub_row_no:sub_row_no+numel(blocks_sub)-1,2) = 1:numel(blocks_sub);
+    datatable_long(sub_row_no:sub_row_no+numel(blocks_sub)-1,3) = blocks_sub';
+    datatable_long(sub_row_no:sub_row_no+numel(blocks_sub)-1,4) = nanmean(ratings_blocks,2);
+        
+    sub_row_no = sub_row_no + 4;
+    
     exp_ratings = ratings_blocks(blocks_sub==1,:);
     exp_ratings_block_mean(sub,:) = nanmean(exp_ratings,2);
     exp_ratings = reshape(exp_ratings',1,[]);
@@ -148,6 +128,50 @@ for sub = 1:numel(subjects)
     
     ratings_allsubs_mean_exp(sub) = nanmean(exp_ratings);
     ratings_allsubs_mean_control(sub) = nanmean(control_ratings);
+    
+    all_trials = 1:numel(ratings_blocks);
+    
+    ratings_blocks_x = reshape(ratings_blocks',1,[])';
+    blocks = reshape(blocks',1,[])';
+    blocktrials = reshape(blocktrials',1,[])';
+    conditions = reshape(conditions',1,[])';
+    datatable_trial(sub_row_no_trial:sub_row_no_trial+numel(all_trials)-1,1) = sub;
+    datatable_trial(sub_row_no_trial:sub_row_no_trial+numel(all_trials)-1,2) = all_trials';
+    datatable_trial(sub_row_no_trial:sub_row_no_trial+numel(all_trials)-1,3) = blocks;
+    datatable_trial(sub_row_no_trial:sub_row_no_trial+numel(all_trials)-1,4) = blocktrials;
+    datatable_trial(sub_row_no_trial:sub_row_no_trial+numel(all_trials)-1,5) = conditions;
+    datatable_trial(sub_row_no_trial:sub_row_no_trial+numel(all_trials)-1,6) = ratings_blocks_x;
+    
+    sub_row_no_trial = sub_row_no_trial + 4*2*9;
+    
+    block_trials = numel(all_trials)/numel(blocks_sub);
+    
+    subplot(rows,cols,sub);
+    for bl = 1:numel(blocks_sub)
+        bl_trials = (block_trials*bl-block_trials):(block_trials*bl);
+        if bl > 1
+            startvalue = ratings_blocks(bl-1,end);
+        else
+            startvalue = NaN;
+        end
+        
+        if blocks_sub(bl) == 1 % experimental blocks
+            plot(bl_trials,[startvalue ratings_blocks(bl,:)],'Color',[239, 123, 5]./255,'LineWidth',1.5);
+        elseif blocks_sub(bl) == 0 % control blocks
+            plot(bl_trials,[startvalue ratings_blocks(bl,:)],'Color',[253, 216, 110]./255,'LineWidth',1.5);
+        end
+        hold on
+        if bl ~= numel(blocks_sub)
+            line([block_trials*bl block_trials*bl],[0 100],'Color','k')
+            hold on
+        end
+    end
+    ylim([0 100])
+    set(gca,'yTick',0:20:100)
+    ylabel('Phasic pain rating (VAS)')
+    xlabel('Trials')
+    set(gca,'xTick',0:10:numel(all_trials))
+    xlim([1 numel(all_trials)])
     
 end
 
@@ -160,6 +184,26 @@ cpm_table = table(SubjectID,Control,Experimental);
 datafile = fullfile(path.main,'data',project.name,project.phase,'cpm_data.csv');
 writetable(cpm_table,datafile)
 
+datatable_long = datatable_long(~isnan(datatable_long(:,1)),:);
+SubjectID = datatable_long(:,1);
+Block = datatable_long(:,2);
+Condition = datatable_long(:,3);
+VAS = datatable_long(:,4);
+cpm_table_long = table(SubjectID,Block,Condition,VAS);
+datafile2 = fullfile(path.main,'data',project.name,project.phase,'cpm_data_long.csv');
+writetable(cpm_table_long,datafile2)
+
+datatable_trial = datatable_trial(~isnan(datatable_trial(:,1)),:);
+SubjectID = datatable_trial(:,1);
+Trial = datatable_trial(:,2);
+Block = datatable_trial(:,3);
+BlockTrial = datatable_trial(:,4);
+Condition = datatable_trial(:,5);
+VAS = datatable_trial(:,6);
+cpm_table_trial = table(SubjectID,Trial,Block,BlockTrial,Condition,VAS);
+datafile3 = fullfile(path.main,'data',project.name,project.phase,'cpm_data_trial.csv');
+writetable(cpm_table_trial,datafile3)
+
 % Calculate within-subject error bars
 subavg = nanmean(cpm_data,2); % mean over conditions for each sub
 grandavg = nanmean(subavg); % mean over subjects and conditions
@@ -167,14 +211,14 @@ grandavg = nanmean(subavg); % mean over subjects and conditions
 newvalues = nan(size(cpm_data));
 
 % normalization of subject values
-for cond = 1:2
-    meanremoved = cpm_data(:,cond)-subavg; % remove mean of conditions from each condition value for each sub
-    newvalues(:,cond) = meanremoved+repmat(grandavg,[numel(subjects) 1 1]); % add grand average over subjects to the values where individual sub average was removed
-    bardata(:,cond) = nanmean(newvalues(:,cond));
+for block = 1:2
+    meanremoved = cpm_data(:,block)-subavg; % remove mean of conditions from each condition value for each sub
+    newvalues(:,block) = meanremoved+repmat(grandavg,[numel(subjects) 1 1]); % add grand average over subjects to the values where individual sub average was removed
+    bardata(:,block) = nanmean(newvalues(:,block));
 end
 
 tvalue = tinv(1-0.025, numel(subjects)-1);
-newvar = (cond/(cond-1))*nanvar(newvalues);
+newvar = (block/(block-1))*nanvar(newvalues);
 errorbars = squeeze(tvalue*(sqrt(newvar)./sqrt(numel(subjects)))); % calculate error bars according to Cousineau (2005) with Morey (2008) fix
     
 % Averaged plot over subjects
@@ -192,8 +236,8 @@ jittered_xdata = xdata + (rand(size(xdata))-0.5)*(2*jitter_amount);
 
 scattercolors = [252, 190, 14; 179, 92, 4]./255;
 
-for cond = 1:2
-    scatter(jittered_xdata(:,cond),cpm_data(:,cond),'filled','MarkerEdgeColor','k','MarkerFaceColor',scattercolors(cond,:));
+for block = 1:2
+    scatter(jittered_xdata(:,block),cpm_data(:,block),'filled','MarkerEdgeColor','k','MarkerFaceColor',scattercolors(block,:));
 end
 
 hold on
